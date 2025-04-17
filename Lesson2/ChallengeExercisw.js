0b1e4128568a10fd8e36d61dee338552dffd9272
Snehalatha-Chevula <chevulasnehalatha@gmail.com>
2025-04-17 15:59:29 +0530

    Final Commit

---
+++ Lesson2/ChallengeExercisw.js
@@ -0,0 +1,6 @@
+//2l.
+console.log((25*9/5)+32);
+//2m.
+console.log((86-32)*5/9);
+//2n.
+console.log((-5*9/5)+32);
---
+++ Lesson2/Exercise.js
@@ -0,0 +1,15 @@
+//2a
+console.log(10+(3*8)+5);
+//2b
+console.log((10+(3*8)+5)/3);
+//2c
+console.log(18.5+(7.5*2));
+//2d
+console.log((10/100)*(18.5+(7.5*2)));
+//2e
+console.log((20/100)*(18.5+(7.5*2)));
+//2j
+console.log(Math.floor(2.8));
+//2k
+console.log(Math.ceil(2.2));
+
